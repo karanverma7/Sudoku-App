35dede30d843760eb12ffa3dca06b73686e20080
Karan Verma <karan.verma@labyrinth.ai>
2021-08-30 20:36:55 +0530

    Added key bindings

---
+++ src/components/styles/sudoku/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const index = styled.div`
+  max-width: 500px;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export default index;
